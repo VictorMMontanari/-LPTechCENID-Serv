9a6c75853cc412c2f467f63b54a1e2234dc2f151
Victor Montanari <victormm9000@gmail.com>
2022-12-14 03:43:13 -0300

    Gerando Token

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mysql = require("mysql");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const e = require("express");
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
 const db = mysql.createPool({
@@ -44,7 +45,53 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/signin", (req, res) => {
+/* ------------------------------###-------------------------------- */
+
+app.post("/signin", (req, res)=> {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(email);
+  console.log(password);
+  db.getConnection ( async (err, db)=> {
+  if (err) throw (err) 
+  const sqlSearch = "SELECT * FROM login WHERE email = ?" 
+  const search_query = mysql.format(sqlSearch,[email])
+  db.query(search_query, async (err, result) => {
+      db.release()
+      console.log(result);
+
+      if (err)
+        throw (err);
+      if (result.length == 0) {
+        console.log("--------> email não existe");
+        res.sendStatus(404);
+      }
+      else {
+        const hasshedPassword = result[0].password;
+        console.log(typeof hasshedPassword);
+        console.log(typeof password);
+        console.log(await bcrypt.compare(password, hasshedPassword));
+        
+        // obter o hashPassword do result
+        if (password === hasshedPassword) {
+          console.log("---------> Login bem-sucedido");
+          console.log("---------> Gerando accessToken"); 
+          const token = jwt.sign({id: result}, 's');
+          console.log(token);
+          /* res.json({ token: token }); */
+        } else {
+          res.send("Senha incorreta!");
+          console.log("Senha incorreta!")
+        } //fim da senha incorreta
+      } // fim do email existe
+    }) //fim da conexão.query() 
+  }) //fim do db.connection()
+  }) //fim do app.post()
+
+/* ------------------------------###-------------------------------- */
+
+/* app.post("/signin", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   console.log(email);
@@ -58,8 +105,8 @@ app.post("/signin", (req, res) => {
     }
     if (result.length > 0) {
       bcrypt.compare(password, result[0].password, (error, response) => {
-        /* console.log(response);
-        console.log(password); */
+        console.log(response);
+        console.log(password);
         console.log(response); 
         if (error) {
           res.send(error);
@@ -74,7 +121,7 @@ app.post("/signin", (req, res) => {
       res.send({ msg: "Usuário não registrado!" });
     }
   });
-});
+}); */
 
 app.listen(3005, () => {
   console.log("rodando na porta 3005");
